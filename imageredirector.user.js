5bbf82ea68ba9f8eaa6bdabfdb22db0bcfc00060
RmG152 <robert-marcos@hotmail.com>
2016-11-15 23:51:55 +0100

    Fix typo
    - 'Missing semicolon. (W033)'
    - Reindent

--- imageredirector.user.js
+++ imageredirector.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name        Image Redirector
 // @namespace   https://github.com/RmG152/ImageRedirectorUserScript/
-// @version     0.1
+// @version     0.1.1
 // @description	Redirect images url from http to relative path
 // ==/UserScript==
 
 
-$('img').prop('src', function () { return this.src.replace(/^http\:/, ''); })
+$('img').prop('src', function() { return this.src.replace(/^http\:/, ''); });
