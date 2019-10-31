59d65d0947c91a02f4868f9af0adb3c3efa0b6a2
RmG152 <robert-marcos@hotmail.com>
2019-10-31 13:12:52 +0100

    Update imageredirector.user.js
    update description

--- imageredirector.user.js
+++ imageredirector.user.js
@@ -5,7 +5,7 @@
 // @author RmG152
 // @updateURL https://github.com/RmG152/ImageRedirectorUserScript/raw/master/imageredirector.meta.js
 // @run-at document-idle
-// @description	Redirect images url from http to relative path http/https
+// @description	Redirect images URL from 'HTTP://' to '//' to solve some issues caused by HTTP Strict Transport Security (HSTS) and third party host.
 // ==/UserScript==
 
 if (window.jQuery) {
