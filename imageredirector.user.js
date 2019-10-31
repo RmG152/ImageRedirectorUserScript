8af6476f8c8aa2d27c147f5a5f55da814b6e77b0
RmG152 <robert-marcos@hotmail.com>
2019-10-31 13:10:40 +0100

    Update imageredirector.user.js
    JS ES7.
    Standard Style

--- imageredirector.user.js
+++ imageredirector.user.js
@@ -1,19 +1,18 @@
 // ==UserScript==
 // @name        Image Redirector
-// @version     0.1.3
+// @version     0.1.4
 // @namespace   https://github.com/RmG152/ImageRedirectorUserScript/
 // @author RmG152
 // @updateURL https://github.com/RmG152/ImageRedirectorUserScript/raw/master/imageredirector.meta.js
 // @run-at document-idle
-// @description	Redirect images url from http to relative path
+// @description	Redirect images url from http to relative path http/https
 // ==/UserScript==
 
 if (window.jQuery) {
-    $('img').prop('src', function() { return this.src.replace(/^http\:/, ''); });
+  $('img').prop('src', function () { return this.src.replace(/^http:/, '') })
 } else {
-    var anchors = document.getElementsByTagName("img");
-
-    for (var i = 0; i < anchors.length; i++) {
-        anchors[i].src = anchors[i].src.replace(/^http\:/, '');
-    }
+  const anchors = document.getElementsByTagName('img')
+  for (let i = 0; i < anchors.length; i++) {
+    anchors[i].src = anchors[i].src.replace(/^http:/, '')
+  }
 }
